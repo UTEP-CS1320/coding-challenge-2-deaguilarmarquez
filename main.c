056fe8ea03b0241b8ed35f964444a93693889760
deaguilarmarquez <70356341+deaguilarmarquez@users.noreply.github.com>
2020-12-12 17:23:36 -0700

    Update main.c

--- main.c
+++ main.c
@@ -16,9 +16,18 @@ int main(void) {
   scanf("%d %d", &s, &d);
 
   // TODO: Complete the rest of the categories
-  if(s < 120) {
+  if((s < 140)||(d >=90)) {
+    printf("Stage 2 Hypertension\n");
+  }else if (s< 120){
     if(d < 80) {
       printf("Normal\n");
+    } }else if ((s >180)||(d >= 120)){
+    printf("Hypertensive Crysis\n");
+  }else if(s >120 && s <129){
+    if(d <80){
+      printf("Elevated\n");
     }
+  }else if ((s >130 && s< 139)||(d >=80 && d<89)){
+    printf("Stage 1 Hypertension\n");
   }
 }
